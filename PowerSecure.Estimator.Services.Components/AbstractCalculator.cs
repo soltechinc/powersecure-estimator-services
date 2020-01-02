b9ffc40d7ca5beebf2cdc200bd2f6d0f6503c32b
John Gibson <john.gibson@soltech.net>
2020-01-02 08:58:18 -0500

    Updates

---
+++ PowerSecure.Estimator.Services.Components/AbstractCalculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PowerSecure.Estimator.Services.Components {
+    public abstract class AbstractCalculator<T> {
+        public abstract T Sum(T paramA, T paramB);
+        public abstract T Difference(T paramA, T paramB);
+        public abstract T Product(T paramA, T paramB);
+        public abstract T Quotient(T paramA, T paramB);
+        public abstract T Max(T paramA, T paramB);
+        public abstract T Min(T paramA, T paramB);
+        public abstract T Ceil(T paramA, T paramB);
+        public abstract T SumProduct(T[] values, T[] factors);
+        public abstract T IsZero(T paramA);
+    }
+}
