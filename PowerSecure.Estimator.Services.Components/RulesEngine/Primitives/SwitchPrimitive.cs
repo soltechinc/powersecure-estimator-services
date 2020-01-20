ca03d357d0193680f58ad1d3b33f4dc264574454
Levi Middleton <levi.middleton@soltech.net>
2020-01-20 13:51:07 -0500

    Added switch primitive

---
+++ PowerSecure.Estimator.Services.Components/RulesEngine/Primitives/SwitchPrimitive.cs
@@ -0,0 +1,78 @@
+﻿//3 parameters - a value, an array of 2-element arrays, each with a value to match and a value to return if matched, and a value to return if there are no matches.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json.Linq;
+using PowerSecure.Estimator.Services.Components.RulesEngine.Repository;
+
+namespace PowerSecure.Estimator.Services.Components.RulesEngine.Primitives
+{
+    public class SwitchPrimitive : IPrimitive
+    {
+        public string Name => "switch";
+
+        public object Invoke(object[] parameters, IReferenceDataRepository referenceDataRepository)
+        {
+            var value = parameters[0].ToComparable();
+            var cases = parameters[1].ToObjectArray();
+
+            foreach(object obj in cases)
+            {
+                var pair = obj.ToObjectArray();
+                if(value.CompareTo(pair[0].ToComparable()) == 0)
+                {
+                    return pair[1];
+                }
+            }
+
+            return parameters[2];
+        }
+
+        public (bool Success, string Message) Validate(JToken jToken)
+        {
+            JToken[] children = jToken.Children().ToArray();
+
+            if (children.Count() != 3)
+            {
+                return (false, $"Expected a parameter array of length 3, got the following: {jToken.Children().Count()}");
+            }
+
+            if (children[0].Type == JTokenType.Array || children[2].Type == JTokenType.Array)
+            {
+                return (false, "Did not expect an array as the first or third parameter.");
+            }
+
+            if (children[1].Type != JTokenType.Array)
+            {
+                return (false, "Expected cases array as the second parameter.");
+            }
+
+            var criteria = children[1].Children();
+            if (criteria.Count() == 0)
+            {
+                return (false, "Expected entries in the cases array.");
+            }
+
+            foreach (JToken jTokenCriteriaPair in criteria)
+            {
+                if (jTokenCriteriaPair.Type != JTokenType.Array)
+                {
+                    return (false, "Expected cases array to contain only key-value pair arrays.");
+                }
+
+                if (jTokenCriteriaPair.Children().Count() != 2)
+                {
+                    return (false, "Expected cases array to contain only key-value pair arrays.");
+                }
+
+                if (jTokenCriteriaPair.Children().Any(p => p.Type == JTokenType.Array))
+                {
+                    return (false, "Expected cases array to contain only key-value pair arrays.");
+                }
+            }
+
+            return (true, string.Empty);
+        }
+    }
+}
---
+++ PowerSecure.Estimator.Services.UnitTest/RulesEngine/Primitives/SwitchPrimitiveTests.cs
@@ -0,0 +1,153 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json.Linq;
+using PowerSecure.Estimator.Services.Components.RulesEngine.Primitives;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PowerSecure.Estimator.Services.UnitTest.RulesEngine.Primitives
+{
+    [TestClass]
+    public class SwitchPrimitiveTests
+    {
+
+        [TestMethod]
+        public void SwitchPrimitive_name()
+        {
+            var primitive = new SwitchPrimitive();
+
+            Assert.AreEqual("switch", primitive.Name, "Switch name changed");
+        }
+
+        [TestMethod]
+        public void SwitchPrimitive_validateTooFewArguments()
+        {
+            var primitive = new SwitchPrimitive();
+
+            (var success, var message) = primitive.Validate(JToken.Parse("[ '2', '2' ]"));
+
+            Assert.IsFalse(success, "Switch arguments (too few) did validate");
+        }
+
+        [TestMethod]
+        public void SwitchPrimitive_validateTooManyArguments()
+        {
+            var primitive = new SwitchPrimitive();
+
+            (var success, var message) = primitive.Validate(JToken.Parse("[ '2', '2', '2', '2' ]"));
+
+            Assert.IsFalse(success, "Switch arguments (too many) did validate");
+        }
+
+        [TestMethod]
+        public void SwitchPrimitive_validate1stArgumentArray()
+        {
+            var primitive = new SwitchPrimitive();
+
+            (var success, var message) = primitive.Validate(JToken.Parse("[ [], '2', '2' ]"));
+
+            Assert.IsFalse(success, "Switch arguments did validate");
+        }
+
+        [TestMethod]
+        public void SwitchPrimitive_validate3rdArgumentArray()
+        {
+            var primitive = new SwitchPrimitive();
+
+            (var success, var message) = primitive.Validate(JToken.Parse("[ '2', '2', [] ]"));
+
+            Assert.IsFalse(success, "Switch arguments did validate");
+        }
+
+        [TestMethod]
+        public void SwitchPrimitive_validateNoArray()
+        {
+            var primitive = new SwitchPrimitive();
+
+            (var success, var message) = primitive.Validate(JToken.Parse("[ '2', '2', '2' ]"));
+
+            Assert.IsFalse(success, "Switch arguments did validate");
+        }
+
+        [TestMethod]
+        public void SwitchPrimitive_validateEmptyArray()
+        {
+            var primitive = new SwitchPrimitive();
+
+            (var success, var message) = primitive.Validate(JToken.Parse("[ '2', [], '2' ]"));
+
+            Assert.IsFalse(success, "Switch arguments did validate");
+        }
+
+        [TestMethod]
+        public void SwitchPrimitive_validateOddArray()
+        {
+            var primitive = new SwitchPrimitive();
+
+            (var success, var message) = primitive.Validate(JToken.Parse("[ '2', [ '3' ], '2' ]"));
+
+            Assert.IsFalse(success, "Switch arguments did validate");
+        }
+
+        [TestMethod]
+        public void SwitchPrimitive_validateCriteriaArrayWithEmptyPair()
+        {
+            var primitive = new SwitchPrimitive();
+
+            (var success, var message) = primitive.Validate(JToken.Parse("[ '2', [ [ ] ], '2' ]"));
+
+            Assert.IsFalse(success, "Switch arguments did validate");
+        }
+
+        [TestMethod]
+        public void SwitchPrimitive_validateCriteriaArrayWithTriad()
+        {
+            var primitive = new SwitchPrimitive();
+
+            (var success, var message) = primitive.Validate(JToken.Parse("[ '2', [ [ '2', '2', '2' ] ], '2' ]"));
+
+            Assert.IsFalse(success, "Switch arguments did validate");
+        }
+
+        [TestMethod]
+        public void SwitchPrimitive_validate()
+        {
+            var primitive = new SwitchPrimitive();
+
+            (var success, var message) = primitive.Validate(JToken.Parse("[ '2', [ ['3', '4'],['5','6'] ], '2' ]"));
+
+            Assert.IsTrue(success, "Switch arguments did not validate");
+        }
+
+        [TestMethod]
+        public void SwitchPrimitive_invokeTrueCase()
+        {
+            var primitive = new SwitchPrimitive();
+
+            var value = primitive.Invoke(new object[] { true, new object[] { new object[] { false, "falseValue" }, new object[] { true, "trueValue" } }, "defaultValue" }, null);
+
+            Assert.AreEqual(value, "trueValue", "True value did not return");
+        }
+
+        [TestMethod]
+        public void SwitchPrimitive_invokeFalseCase()
+        {
+            var primitive = new SwitchPrimitive();
+
+            var value = primitive.Invoke(new object[] { false, new object[] { new object[] { false, "falseValue" }, new object[] { true, "trueValue" } }, "defaultValue" }, null);
+
+            Assert.AreEqual(value, "falseValue", "False value did not return");
+        }
+
+        [TestMethod]
+        public void SwitchPrimitive_invokeDefaultCase()
+        {
+            var primitive = new SwitchPrimitive();
+
+            var value = primitive.Invoke(new object[] { 1, new object[] { new object[] { 2, "falseValue" }, new object[] { 3, "trueValue" } }, "defaultValue" }, null);
+
+            Assert.AreEqual(value, "defaultValue", "Default value did not return");
+        }
+    }
+}
