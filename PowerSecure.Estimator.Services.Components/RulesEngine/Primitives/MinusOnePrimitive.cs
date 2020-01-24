4d465f72c54a75a1ec1a41e4d03edf73c846302a
John Gibson <john.gibson@soltech.net>
2020-01-24 16:09:28 -0500

    Added MinusOnePrimitive

---
+++ PowerSecure.Estimator.Services.Components/RulesEngine/Primitives/MinusOnePrimitive.cs
@@ -0,0 +1,37 @@
+﻿// 1 parameter
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json.Linq;
+using PowerSecure.Estimator.Services.Components.RulesEngine.Repository;
+using System.Linq;
+
+namespace PowerSecure.Estimator.Services.Components.RulesEngine.Primitives
+{
+    public class MinusOnePrimitive : IPrimitive
+    {
+        public string Name => "--";
+        
+        public object Invoke(object[] parameters, IReferenceDataRepository referenceDataRepository)
+        {
+            var value = parameters.ToDecimal().First();
+
+            return value - 1;
+        }
+
+        public (bool Success, string Message) Validate(JToken jToken)
+        {
+            if (jToken.Children().Count() != 1)
+            {
+                return (false, $"Expected a parameter array of length 1, got the following: {jToken.Children().Count()}");
+            }
+
+            if (jToken.Children().Any(p => p.Type == JTokenType.Array))
+            {
+                return (false, "Did not expect any arrays as parameters.");
+            }
+
+            return (true, string.Empty);
+        }
+    }
+}
