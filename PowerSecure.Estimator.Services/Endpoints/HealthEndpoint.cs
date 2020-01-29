acdac2234caa73beb215aa028d611da8273aaba4
Levi Middleton <levi.middleton@soltech.net>
2020-01-29 13:05:13 -0500

    Added health check endpoint

---
+++ PowerSecure.Estimator.Services/Endpoints/HealthEndpoint.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using PowerSecure.Estimator.Services.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowerSecure.Estimator.Services.Endpoints
+{
+    public static class HealthEndpoint
+    {
+        [FunctionName("CheckProperties")]
+        public static async Task<IActionResult> CheckProperties(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "health/properties")] HttpRequest req,
+            ILogger log)
+        {
+            try
+            {
+                log.LogTrace("Function called - ListModules");
+
+                return (await new HealthService().CheckProperties()).ToOkObjectResult();
+            }
+            catch (Exception ex)
+            {
+                log.LogError(ex, "Caught exception");
+                return ex.ToServerErrorObjectResult(ex.Message);
+            }
+        }
+    }
+}
---
+++ PowerSecure.Estimator.Services/Services/HealthService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowerSecure.Estimator.Services.Services
+{
+    public partial class HealthService
+    {
+        public async Task<object> CheckProperties()
+        {
+            var missingProperties = new List<string>(MissingPropertiesIterator());
+
+            return missingProperties;
+        }
+    }
+}
