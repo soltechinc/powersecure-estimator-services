2236184d9a8355cde37a65bf6933e5316f8a9e2c
Levi Middleton <levi.middleton@soltech.net>
2020-02-26 08:21:06 -0500

    Added conversion from ui input to data sheet

---
+++ PowerSecure.Estimator.Services.UnitTest/Services/EstimateServiceTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json.Linq;
+using PowerSecure.Estimator.Services.Services;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace PowerSecure.Estimator.Services.UnitTest.Services
+{
+    [TestClass]
+    public class EstimateServiceTests
+    {
+        [TestMethod]
+        public void HappyPathTest_singleInstruction()
+        {
+            var estimateService = new EstimateService(null);
+
+            JObject inputFromUi = JObject.Parse(File.ReadAllText(@".\Resources\module_evaluate_sample_json1.json"));
+
+            estimateService.Evaluate(inputFromUi);
+        }
+    }
+}
--- PowerSecure.Estimator.Services/Services/EstimateService.cs
+++ PowerSecure.Estimator.Services/Services/EstimateService.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PowerSecure.Estimator.Services.Components.RulesEngine;
 
 namespace PowerSecure.Estimator.Services.Services
 {
@@ -19,6 +20,54 @@ namespace PowerSecure.Estimator.Services.Services
 
         public async Task<(object, string)> Evaluate(JObject uiInputs)
         {
+            var dataSheet = new Dictionary<string, object>();
+            string moduleName = uiInputs.Properties().Where(prop => prop.Name == "moduleTitle").First().Value.ToObject<string>().ToLower().Trim();
+            //TODO - Translate into data sheet
+            uiInputs.WalkNodes(PreOrder: jToken =>
+                {
+                    switch(jToken)
+                    {
+                        case JObject jObject:
+                            {
+                                if(!jObject.Properties().Any(prop => prop.Name == "input"))
+                                {
+                                    break;
+                                }
+
+                                bool isInput = jObject.Properties().Where(prop => prop.Name == "input").First().Value.ToObject<bool>();
+                                string name = jObject.Properties().Where(prop => prop.Name == "variableName").First().Value.ToObject<string>().ToLower().Trim();
+                                object inputValue;
+                                JToken inputValueFromJson = jObject.Properties().Where(prop => prop.Name == "inputValue").First().Value;
+                                switch(inputValueFromJson.Type)
+                                {
+                                    case JTokenType.Integer:
+                                        {
+                                            inputValue = Convert.ToDecimal(inputValueFromJson.ToObject<int>());
+                                            break;
+                                        }
+                                    case JTokenType.Float:
+                                        {
+                                            inputValue = Convert.ToDecimal(inputValueFromJson.ToObject<float>());
+                                            break;
+                                        }
+                                    case JTokenType.Boolean:
+                                        {
+                                            inputValue = inputValueFromJson.ToObject<bool>();
+                                            break;
+                                        }
+                                    default:
+                                        {
+                                            inputValue = inputValueFromJson.ToObject<string>();
+                                            break;
+                                        }
+                                }
+
+                                dataSheet.Add($"{moduleName}.{name}", isInput ? inputValue : null);
+                                break;
+                            }
+                    }
+                });
+
             //TODO - Add functionality
             return (null, "");
         }
