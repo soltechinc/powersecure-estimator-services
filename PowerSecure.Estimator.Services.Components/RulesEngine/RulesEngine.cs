3914a8a5bc1b2d762cd042520c1146894c8de9ae
Levi Middleton <levi.middleton@soltech.net>
2020-02-26 07:04:19 -0500

    Revert "Added estimate endpoint with evaluate method"
    This reverts commit 54e39545f8a1daaac9783eda1b98c1b7301d20ff.

--- PowerSecure.Estimator.Services.Components/RulesEngine/RulesEngine.cs
+++ PowerSecure.Estimator.Services.Components/RulesEngine/RulesEngine.cs
@@ -5,140 +5,97 @@ using System.Text;
 using System.Linq;
 using PowerSecure.Estimator.Services.Components.RulesEngine.Primitives;
 using PowerSecure.Estimator.Services.Components.RulesEngine.Conversions;
-using Newtonsoft.Json.Linq;
 
 namespace PowerSecure.Estimator.Services.Components.RulesEngine {
     public class RulesEngine : IRulesEngine
     {
-        public IDictionary<string, object> EvaluateDataSheet(string dataSheet, DateTime effectiveDate, IDictionary<string, IFunction> functions, IInstructionSetRepository instructionSetRepository, IReferenceDataRepository referenceDataRepository)
+        public IDictionary<string, object> EvaluateDataSheet(IDictionary<string, object> dataSheet, DateTime effectiveDate, IDictionary<string, IFunction> functions, IInstructionSetRepository instructionSetRepository, IReferenceDataRepository referenceDataRepository)
         {
             var suppliedParameters = new HashSet<string>();
             var missingParameters = new HashSet<string>();
 
-            var stack = new Stack<Dictionary<string, object>>();
-            stack.Push(new Dictionary<string, object>());
-            JObject.Parse(dataSheet).WalkNodes(
-                PreOrder: jToken =>
-                {
-                    switch (jToken)
-                    {
-                        case JObject jObject:
-                            {
-                                var parameters = stack.Peek();
-                                jObject.Properties()
-                                    .Where(p => p.Value.Type != JTokenType.Object && p.Value.Type != JTokenType.Array)
-                                    .Aggregate(parameters, (acc, prop) =>
-                                     {
-                                         switch (prop.Value.Type)
-                                         {
-                                             case JTokenType.Boolean:
-                                                 acc.Add(prop.Path, prop.Value.ToObject<bool>());
-                                                 break;
-                                             case JTokenType.Integer:
-                                                 acc.Add(prop.Path, Convert.ToDecimal(prop.Value.ToObject<int>()));
-                                                 break;
-                                             case JTokenType.Float:
-                                                 acc.Add(prop.Path, Convert.ToDecimal(prop.Value.ToObject<float>()));
-                                                 break;
-                                             case JTokenType.Null:
-                                                 acc.Add(prop.Path, null);
-                                                 break;
-                                             default:
-                                                 acc.Add(prop.Path, prop.Value.ToString());
-                                                 break;
-                                         }
-                                         return acc;
-                                     });
-                                break;
-                            }
-                        case JArray jArray:
-                            {
-                                
-                                break;
-                            }
-                    }
-                });
+            foreach(var parameter in dataSheet)
             {
-                var parameters = stack.Pop();
-                /*
-                foreach (var parameter in dataSheet)
+                if(parameter.Value == null)
                 {
-                    if(parameter.Value == null)
-                    {
-                        missingParameters.Add(parameter.Key.Trim().ToLower());
-                    }
-                    else
-                    {
-                        suppliedParameters.Add(parameter.Key.Trim().ToLower());
-                    }
-                    parameters.Add(parameter.Key?.Trim()?.ToLower(), parameter.Value);
-                }*/
-
-                var childInstructionSetKeys = new HashSet<string>();
-                var instructionSets = new Dictionary<string, IInstructionSet>();
-                var neededParameters = new HashSet<string>();
-                foreach (var instructionSet in instructionSetRepository.SelectByKey(missingParameters, effectiveDate))
-                {
-                    instructionSets.Add(instructionSet.Name, instructionSet);
-                    foreach (var childInstructionSetKey in instructionSet.ChildInstructionSets)
-                    {
-                        if (!childInstructionSetKeys.Contains(childInstructionSetKey) && !suppliedParameters.Contains(childInstructionSetKey))
-                        {
-                            childInstructionSetKeys.Add(childInstructionSetKey);
-                        }
-                    }
-                    foreach (var parameter in instructionSet.Parameters)
-                    {
-                        if (!neededParameters.Contains(parameter))
-                        {
-                            neededParameters.Add(parameter);
-                        }
-                    }
+                    missingParameters.Add(parameter.Key.Trim().ToLower());
                 }
-
-                foreach (var instructionSet in instructionSetRepository.SelectByKey(childInstructionSetKeys, effectiveDate))
+                else
                 {
-                    if (!instructionSets.ContainsKey(instructionSet.Key))
-                    {
-                        instructionSets.Add(instructionSet.Key, instructionSet);
-                    }
-                    foreach (var parameter in instructionSet.Parameters)
-                    {
-                        if (!neededParameters.Contains(parameter))
-                        {
-                            neededParameters.Add(parameter);
-                        }
-                    }
-                }
-
-                if (!neededParameters.All(p => suppliedParameters.Contains(p)))
-                {
-                    throw new InvalidOperationException("Missing required parameters");
-                }
-
-                foreach (var instructionSet in instructionSets.Values)
-                {
-                    if (!parameters.ContainsKey(instructionSet.Key))
-                    {
-                        parameters.Add(instructionSet.Key, instructionSet);
-                    }
-                    else if (parameters[instructionSet.Key] == null)
-                    {
-                        parameters[instructionSet.Key] = instructionSet;
-                    }
-                }
-
-                foreach (var key in missingParameters)
-                {
-                    if (parameters[key] is IInstructionSet instructionSet)
-                    {
-                        parameters[key] = instructionSet.Evaluate(parameters, functions, referenceDataRepository);
-                    }
-                    //dataSheet[key] = parameters[key].ToRawString();
+                    suppliedParameters.Add(parameter.Key.Trim().ToLower());
                 }
             }
 
-            return null;
+            var childInstructionSetKeys = new HashSet<string>();
+            var instructionSets = new Dictionary<string, IInstructionSet>();
+            var neededParameters = new HashSet<string>();
+            foreach(var instructionSet in instructionSetRepository.SelectByKey(missingParameters, effectiveDate))
+            {
+                instructionSets.Add(instructionSet.Name, instructionSet);
+                foreach(var childInstructionSetKey in instructionSet.ChildInstructionSets)
+                {
+                    if(!childInstructionSetKeys.Contains(childInstructionSetKey) && !suppliedParameters.Contains(childInstructionSetKey))
+                    {
+                        childInstructionSetKeys.Add(childInstructionSetKey);
+                    }
+                }
+                foreach(var parameter in instructionSet.Parameters)
+                {
+                    if(!neededParameters.Contains(parameter))
+                    {
+                        neededParameters.Add(parameter);
+                    }
+                }
+            }
+
+            foreach(var instructionSet in instructionSetRepository.SelectByKey(childInstructionSetKeys, effectiveDate))
+            {
+                if(!instructionSets.ContainsKey(instructionSet.Key))
+                {
+                    instructionSets.Add(instructionSet.Key, instructionSet);
+                }
+                foreach (var parameter in instructionSet.Parameters)
+                {
+                    if (!neededParameters.Contains(parameter))
+                    {
+                        neededParameters.Add(parameter);
+                    }
+                }
+            }
+
+            if(!neededParameters.All(p => suppliedParameters.Contains(p)))
+            {
+                throw new InvalidOperationException("Missing required parameters");
+            }
+
+            var parameters = new Dictionary<string, object>();
+            foreach(var pair in dataSheet)
+            {
+                parameters.Add(pair.Key?.Trim()?.ToLower(), pair.Value);
+            }
+            
+            foreach (var instructionSet in instructionSets.Values)
+            {
+                if(!parameters.ContainsKey(instructionSet.Key))
+                {
+                    parameters.Add(instructionSet.Key, instructionSet);
+                }
+                else if (parameters[instructionSet.Key] == null)
+                {
+                    parameters[instructionSet.Key] = instructionSet;
+                }
+            }
+
+            foreach (var key in missingParameters)
+            {
+                if (parameters[key] is IInstructionSet instructionSet)
+                {
+                    parameters[key] = instructionSet.Evaluate(parameters, functions, referenceDataRepository);
+                }
+                dataSheet[key] = parameters[key].ToRawString();
+            }
+
+            return dataSheet;
         }
     }
 }
--- PowerSecure.Estimator.Services.UnitTest/RulesEngine/RulesEngine.FunctionalTests.cs
+++ PowerSecure.Estimator.Services.UnitTest/RulesEngine/RulesEngine.FunctionalTests.cs
@@ -21,25 +21,8 @@ namespace PowerSecure.Estimator.Services.UnitTest.RulesEngine
             
             var engine = new Components.RulesEngine.RulesEngine();
 
-            var dataSheet = engine.EvaluateDataSheet("{ \"x\" : 2, \"y\" : \"3\", \"all.test\" : null }", DateTime.Now, primitives, repository, null);
-
-
-            Assert.AreEqual(3, dataSheet.Count, "Count of items in data sheet is incorrect");
-            Assert.AreEqual("12", dataSheet["all.test"], "Calculation is incorrect");
-        }
-
-        [TestMethod]
-        public void HappyPathTest_nestedDataSheet()
-        {
-            var repository = new InMemoryInstructionSetRepository();
-            var primitives = Primitive.Load();
-            repository.InsertNew("All", "test", " { '*': [ 'y', { '+': [ 'x', 2 ] } ]} ", DateTime.MinValue, DateTime.Now, TestInstructionSet.Create, primitives);
-
-            var engine = new Components.RulesEngine.RulesEngine();
-
-            var dataSheet = engine.EvaluateDataSheet("{ \"x\" : 2, \"y\" : \"3\", \"all\" : { \"test\" : null }, \"z\" : [{ \"a\" : 3 },{ \"a\" : 4 }] }", DateTime.Now, primitives, repository, null);
-
-
+            var dataSheet = engine.EvaluateDataSheet(new Dictionary<string, object> { ["x"] = "2", ["y"] = "3", ["all.test"] = null }, DateTime.Now, primitives, repository, null);
+            
             Assert.AreEqual(3, dataSheet.Count, "Count of items in data sheet is incorrect");
             Assert.AreEqual("12", dataSheet["all.test"], "Calculation is incorrect");
         }
@@ -54,7 +37,7 @@ namespace PowerSecure.Estimator.Services.UnitTest.RulesEngine
 
             var engine = new Components.RulesEngine.RulesEngine();
 
-            var dataSheet = engine.EvaluateDataSheet("{ \"x\" : \"2\", \"y\" : \"3\", \"all.test2\" : null }", DateTime.Now, primitives, repository, null);
+            var dataSheet = engine.EvaluateDataSheet(new Dictionary<string, object> { ["x"] = 2, ["y"] = "3", ["all.test2"] = null }, DateTime.Now, primitives, repository, null);
 
             Assert.AreEqual(3, dataSheet.Count, "Count of items in data sheet is incorrect");
             Assert.AreEqual("36", dataSheet["all.test2"], "Calculation is incorrect");
@@ -71,7 +54,7 @@ namespace PowerSecure.Estimator.Services.UnitTest.RulesEngine
 
             var engine = new Components.RulesEngine.RulesEngine();
 
-            var dataSheet = engine.EvaluateDataSheet("{ \"x\" : \"2\", \"y\" : \"3\", \"z\" : \"nan\", \"all.test2\" : null }", DateTime.Now, primitives, repository, null);
+            var dataSheet = engine.EvaluateDataSheet(new Dictionary<string, object> { ["x"] = 2, ["y"] = "3", ["z"] = "nan", ["all.test2"] = null }, DateTime.Now, primitives, repository, null);
 
             Assert.AreEqual(4, dataSheet.Count, "Count of items in data sheet is incorrect");
             Assert.AreEqual("12", dataSheet["all.test2"], "Calculation is incorrect");
@@ -86,7 +69,7 @@ namespace PowerSecure.Estimator.Services.UnitTest.RulesEngine
 
             var engine = new Components.RulesEngine.RulesEngine();
 
-            var dataSheet = engine.EvaluateDataSheet("{ \"x\" : \"2\", \"y\" : \"3\", \"all.test\" : null }", DateTime.Now, primitives, repository, null);
+            var dataSheet = engine.EvaluateDataSheet(new Dictionary<string, object> { ["x"] = "2", ["y"] = "3", ["all.test"] = null }, DateTime.Now, primitives, repository, null);
 
             Assert.AreEqual(3, dataSheet.Count, "Count of items in data sheet is incorrect");
             Assert.AreEqual("15", dataSheet["all.test"], "Calculation is incorrect");
@@ -104,7 +87,7 @@ namespace PowerSecure.Estimator.Services.UnitTest.RulesEngine
 
             var engine = new Components.RulesEngine.RulesEngine();
 
-            var dataSheet = engine.EvaluateDataSheet("{ \"x\" : \"2\", \"y\" : \"3\", \"z\" : \"nan\", \"all.test3\" : null }", DateTime.Now, primitives, repository, null);
+            var dataSheet = engine.EvaluateDataSheet(new Dictionary<string, object> { ["x"] = 2, ["y"] = "3", ["z"] = "nan", ["all.test3"] = null }, DateTime.Now, primitives, repository, null);
 
             Assert.AreEqual(4, dataSheet.Count, "Count of items in data sheet is incorrect");
             Assert.AreEqual("12", dataSheet["all.test2"], "Calculation is incorrect");
@@ -121,7 +104,7 @@ namespace PowerSecure.Estimator.Services.UnitTest.RulesEngine
 
             var engine = new Components.RulesEngine.RulesEngine();
 
-            var dataSheet = engine.EvaluateDataSheet("{ \"x\" : \"2\", \"all.test2\" : null }", DateTime.Now, primitives, repository, null);
+            var dataSheet = engine.EvaluateDataSheet(new Dictionary<string, object> { ["x"] = "2", ["all.test2"] = null }, DateTime.Now, primitives, repository, null);
         }
 
         [TestMethod]
@@ -134,7 +117,7 @@ namespace PowerSecure.Estimator.Services.UnitTest.RulesEngine
 
             var engine = new Components.RulesEngine.RulesEngine();
 
-            var dataSheet = engine.EvaluateDataSheet("{ \"all.test\" : 4, \"all.test\" : null }", DateTime.Now, primitives, repository, null);
+            var dataSheet = engine.EvaluateDataSheet(new Dictionary<string, object> { ["all.test"] = 4, ["all.test2"] = null }, DateTime.Now, primitives, repository, null);
 
             Assert.AreEqual(2, dataSheet.Count, "Count of items in data sheet is incorrect");
             Assert.AreEqual("12", dataSheet["all.test2"], "Calculation is incorrect");
@@ -149,7 +132,7 @@ namespace PowerSecure.Estimator.Services.UnitTest.RulesEngine
 
             var engine = new Components.RulesEngine.RulesEngine();
 
-            var dataSheet = engine.EvaluateDataSheet("{ \"x\" : \"2\", \"y\" : \"3\", \"all.test\" : 5 }", DateTime.Now, primitives, repository, null);
+            var dataSheet = engine.EvaluateDataSheet(new Dictionary<string, object> { ["x"] = "2", ["y"] = "3", ["all.test"] = "5" }, DateTime.Now, primitives, repository, null);
 
             Assert.AreEqual(3, dataSheet.Count, "Count of items in data sheet is incorrect");
             Assert.AreEqual("5", dataSheet["all.test"], "Value of test was changed");
