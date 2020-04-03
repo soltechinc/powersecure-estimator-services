e640a2072f59c4dd8de137c80a87c08783533879
John Gibson <john.gibson@soltech.net>
2020-04-02 20:47:57 -0400

    BO updates

--- PowerSecure.Estimator.Services/Models/BusinessOpportunity.cs
+++ PowerSecure.Estimator.Services/Models/BusinessOpportunity.cs
@@ -3,23 +3,21 @@ using System;
 
 namespace PowerSecure.Estimator.Services.Models {
     public class BusinessOpportunity {
-        [JsonProperty("id")]
-        public string Id { get; set; }
 
         [JsonProperty("ifsboNumber")]
         public string IFSBONumber { get; set; }
 
-        [JsonProperty("name")]
-        public string Name { get; set; }
+        [JsonProperty("opportunityName")]
+        public string OpportunityName { get; set; }
 
         [JsonProperty("status")]
         public string Status { get; set; }
 
-        [JsonProperty("market")]
-        public string VerticalMarket { get; set; }
-
         [JsonProperty("created")]
         public DateTime Created { get; set; }
+
+        [JsonProperty("dateDue")]
+        public DateTime DateDue { get; set; }
 
         [JsonProperty("createdBy")]
         public string CreatedBy { get; set; }
--- PowerSecure.Estimator.Services/Repositories/CosmosBusinessOpportunityRepository.cs
+++ PowerSecure.Estimator.Services/Repositories/CosmosBusinessOpportunityRepository.cs
@@ -25,7 +25,7 @@ namespace PowerSecure.Estimator.Services.Repositories {
 
         public async Task<int> Delete(string ifsBONumber, IDictionary<string, string> queryParams) {
             if (queryParams.ContainsKey(_queryParamsKey)) {
-                await _dbClient.DeleteDocumentAsync(UriFactory.CreateDocumentUri(databaseId: _databaseId, collectionId: _collectionId, documentId: queryParams["id"]),
+                await _dbClient.DeleteDocumentAsync(UriFactory.CreateDocumentUri(databaseId: _databaseId, collectionId: _collectionId, documentId: queryParams[_queryParamsKey]),
                     new RequestOptions { PartitionKey = new PartitionKey(ifsBONumber) });
                 return 1;
             }
@@ -37,7 +37,7 @@ namespace PowerSecure.Estimator.Services.Repositories {
             var list = new List<Document>();
             while (query.HasMoreResults) {
                 foreach (BusinessOpportunity businessOpportunity in await query.ExecuteNextAsync()) {
-                    list.Add(await _dbClient.DeleteDocumentAsync(UriFactory.CreateDocumentUri(databaseId: _databaseId, collectionId: _collectionId, documentId: businessOpportunity.Id), 
+                    list.Add(await _dbClient.DeleteDocumentAsync(UriFactory.CreateDocumentUri(databaseId: _databaseId, collectionId: _collectionId, documentId: businessOpportunity.IFSBONumber), 
                         new RequestOptions { PartitionKey = new PartitionKey(businessOpportunity.IFSBONumber) }));
                 }
             }
@@ -47,7 +47,7 @@ namespace PowerSecure.Estimator.Services.Repositories {
 
         public async Task<object> Get(string ifsBONumber, IDictionary<string, string> queryParams) {
             if (queryParams.ContainsKey(_queryParamsKey)) {
-                return (Document)await _dbClient.ReadDocumentAsync(UriFactory.CreateDocumentUri(databaseId: _databaseId, collectionId: _collectionId, documentId: queryParams["id"]), 
+                return (Document)await _dbClient.ReadDocumentAsync(UriFactory.CreateDocumentUri(databaseId: _databaseId, collectionId: _collectionId, documentId: queryParams[_queryParamsKey]), 
                     new RequestOptions { PartitionKey = new PartitionKey(queryParams[_queryParamsKey]) });
             }
 
@@ -87,8 +87,8 @@ namespace PowerSecure.Estimator.Services.Repositories {
         }
 
         public async Task<object> Upsert(JObject document) {
-            if (document.ContainsKey("id")) {
-                return (Document)await _dbClient.ReplaceDocumentAsync(UriFactory.CreateDocumentUri(databaseId: _databaseId, collectionId: _collectionId, documentId: document["id"].ToString()), document, new RequestOptions { PartitionKey = new PartitionKey(document["module"].ToString()) });
+            if (document.ContainsKey(_queryParamsKey)) {
+                return (Document)await _dbClient.ReplaceDocumentAsync(UriFactory.CreateDocumentUri(databaseId: _databaseId, collectionId: _collectionId, documentId: document[_queryParamsKey].ToString()), document, new RequestOptions { PartitionKey = new PartitionKey(document["module"].ToString()) });
             }
 
             return (Document)await _dbClient.CreateDocumentAsync(UriFactory.CreateDocumentCollectionUri(databaseId: _databaseId, collectionId: _collectionId), document);
