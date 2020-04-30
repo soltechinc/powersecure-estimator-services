9b2c7d09f1d77cbdac691c07e2f42a1050251583
John Gibson <john.gibson@soltech.net>
2020-04-30 07:56:56 -0400

    Estimate updates

--- PowerSecure.Estimator.Services/Repositories/CosmosEstimateRepository.cs
+++ PowerSecure.Estimator.Services/Repositories/CosmosEstimateRepository.cs
@@ -43,10 +43,10 @@ namespace PowerSecure.Estimator.Services.Repositories {
         }
 
         public async Task<object> Upsert(JObject document) {
-            //if (document.ContainsKey("id")) {
+            if (document.ContainsKey("id")) {
                 document = await UpdateNumber(document);
-              //return (Document)await _dbClient.ReplaceDocumentAsync(UriFactory.CreateDocumentUri(databaseId: _databaseId, collectionId: _collectionId, documentId: document["id"].ToString()), document, new RequestOptions { PartitionKey = new PartitionKey(document["boliNumber"].ToString()) });
-            //}
+              return (Document)await _dbClient.ReplaceDocumentAsync(UriFactory.CreateDocumentUri(databaseId: _databaseId, collectionId: _collectionId, documentId: document["id"].ToString()), document, new RequestOptions { PartitionKey = new PartitionKey(document["boliNumber"].ToString()) });
+            }
 
             return (Document)await _dbClient.CreateDocumentAsync(UriFactory.CreateDocumentCollectionUri(databaseId: _databaseId, collectionId: _collectionId), document);
         }
