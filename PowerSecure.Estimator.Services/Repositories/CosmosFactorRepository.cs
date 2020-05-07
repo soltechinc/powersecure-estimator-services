43e0499523baadc1d1dc11eb208e90cff90a52a8
Levi Middleton <levi.middleton@soltech.net>
2020-05-07 10:11:53 -0400

    Added null id checks for factors and functions.

--- PowerSecure.Estimator.Services/Repositories/CosmosFactorRepository.cs
+++ PowerSecure.Estimator.Services/Repositories/CosmosFactorRepository.cs
@@ -71,7 +71,7 @@ namespace PowerSecure.Estimator.Services.Repositories
 
         public async Task<object> Upsert(JObject document)
         {
-            if(document.ContainsKey("id"))
+            if (document.ContainsKey("id") && document["id"] != null && !string.IsNullOrEmpty(document["id"].ToString()))
             {
                 return (Document)await _dbClient.ReplaceDocumentAsync(UriFactory.CreateDocumentUri(databaseId: _databaseId, collectionId: _collectionId, documentId: document["id"].ToString()), document, new RequestOptions { PartitionKey = new PartitionKey(document["module"].ToString()) });
             }
