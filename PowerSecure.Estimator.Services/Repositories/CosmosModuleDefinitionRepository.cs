4a0ecbef1b66db3e51ff33e3fb3f7af4c7af3e3c
John Gibson <john.gibson@soltech.net>
2020-06-02 14:57:50 -0400

    Update key for moduledefinition

--- PowerSecure.Estimator.Services/Repositories/CosmosModuleDefinitionRepository.cs
+++ PowerSecure.Estimator.Services/Repositories/CosmosModuleDefinitionRepository.cs
@@ -22,7 +22,7 @@ namespace PowerSecure.Estimator.Services.Repositories {
         }
 
         public async Task<object> Upsert(JObject document) {
-           if (document.ContainsKey("id")) {
+           if (document.ContainsKey("moduleid")) {
                 return (Document)await _dbClient.ReplaceDocumentAsync(UriFactory.CreateDocumentUri(databaseId: _databaseId, collectionId: _collectionId, documentId: document["id"].ToString()), document, new RequestOptions { PartitionKey = new PartitionKey(document["moduleId"].ToString()) });
             }
             return (Document)await _dbClient.CreateDocumentAsync(UriFactory.CreateDocumentCollectionUri(databaseId: _databaseId, collectionId: _collectionId), document);
