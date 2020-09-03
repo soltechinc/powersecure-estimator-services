aad330a8bb34fc13064d2d6653f209a00d71820a
Levi Middleton <levi.middleton@soltech.net>
2020-09-03 07:14:26 -0400

    Added new file upload uri

--- PowerSecure.Estimator.Services/Endpoints/BlobStorageEndpoint.cs
+++ PowerSecure.Estimator.Services/Endpoints/BlobStorageEndpoint.cs
@@ -5,18 +5,25 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
 using PowerSecure.Estimator.Services.ActionResults;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using PowerSecure.Estimator.Services.Services;
 using Newtonsoft.Json;
 using static PowerSecure.Estimator.Services.Shared.DTOs;
+using System.IO;
+using Newtonsoft.Json.Linq;
+using System.Linq;
 
-namespace PowerSecure.Estimator.Services.Endpoints {
-    public static class BlobStorageEndpoint {
-        
-    [FunctionName("GetABS")]
+namespace PowerSecure.Estimator.Services.Endpoints
+{
+    public static class BlobStorageEndpoint
+    {
+        [FunctionName("GetABS")]
         public static async Task<IActionResult> Get(
-        [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "abs")] HttpRequest req, ILogger log) {
-            try {
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "abs")] HttpRequest req, ILogger log)
+        {
+            try
+            {
                 log.LogDebug("Function called - GetABS");
                 var abs = new ABSDTO();
                 abs.blobStorageAccountName = AppSettings.Get("BlobStorageAccountName");
@@ -25,10 +32,68 @@ namespace PowerSecure.Estimator.Services.Endpoints {
                 abs.sasToken = AppSettings.Get("StorageAccountSASToken");
                 string results = JsonConvert.SerializeObject(abs);
                 return new JsonResult(results);
-            } catch (Exception ex) {
+            }
+            catch (Exception ex)
+            {
                 log.LogError(ex, "Caught exception");
                 return new object().ToServerErrorObjectResult();
             }
+        }
+
+        [FunctionName("UploadFile")]
+        public static async Task<IActionResult> UploadFile(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "files")] HttpRequest req, ILogger log)
+        {
+
+            try
+            {
+                log.LogDebug("Function called - UploadFile");
+                var dict = new Dictionary<string, object>();
+                foreach(var pair in req.Form)
+                {
+                    dict.Add(pair.Key, pair.Value.ToArray());
+                }
+
+                var list = new List<byte[]>();
+                if(req.Form.Files != null)
+                {
+                    foreach(var file in req.Form.Files)
+                    {
+                        using (MemoryStream mem = new MemoryStream())
+                        {
+                            file.CopyTo(mem);
+                            list.Add(mem.ToArray());
+                        }
+                    }
+                }
+
+                log.LogInformation("Key/value data: " + JToken.FromObject(dict));
+                log.LogInformation($"Found {list.Count} files with sizes {string.Join(",",list.Select(x => x.Length.ToString()))}");
+
+                return "".ToOkObjectResult();
+            }
+            catch (Exception ex)
+            {
+                log.LogError(ex, "Caught exception");
+                return new object().ToServerErrorObjectResult();
+            }
+            /*
+            try
+            {
+                log.LogDebug("Function called - UploadFile");
+                var abs = new ABSDTO();
+                abs.blobStorageAccountName = AppSettings.Get("BlobStorageAccountName");
+                abs.blobStorageConnectionString = AppSettings.Get("BlobStorageConnectionString");
+                abs.blobStorageKey = AppSettings.Get("BlobStorageKey");
+                abs.sasToken = AppSettings.Get("StorageAccountSASToken");
+                string results = JsonConvert.SerializeObject(abs);
+                return new JsonResult(results);
+            }
+            catch (Exception ex)
+            {
+                log.LogError(ex, "Caught exception");
+                return new object().ToServerErrorObjectResult();
+            }*/
         }
     }
 }
